cd3bdaac33d231bf5fda285d98ea9fc6f0409186
chenaski <mail2chenaski@gmail.com>
2022-03-08 16:42:14 +0300

    feat: add initial script, image to test

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import {ImagePool} from '@squoosh/lib';
+import {cpus} from 'os';
+import fs from "fs/promises";
+import path from "path";
+
+export async function easyimg(options) {
+  const imagePool = new ImagePool(cpus().length);
+  const file = await fs.readFile(options.filePath);
+  const {name: srcFileName} = path.parse(options.filePath);
+  const image = imagePool.ingestImage(file);
+
+  const encodeOptions = {
+    oxipng: {},
+    mozjpeg: {},
+    webp: {},
+    avif: {},
+    jxl: {},
+  };
+  const encodedImage = await image.encode(encodeOptions);
+
+  for (const encodedImage of Object.values(image.encodedWith)) {
+    await fs.mkdir(options.outDir, {recursive: true});
+    const resultImage = await encodedImage;
+    await fs.writeFile(`${path.resolve(options.outDir, srcFileName)}.${resultImage.extension}`, resultImage.binary);
+  }
+
+  await imagePool.close();
+}
+
+(async () => {
+  const options = {filePath: process.argv[2], outDir: process.argv[3]};
+  await easyimg(options);
+})()
