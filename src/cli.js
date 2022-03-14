43eecb17974654135b44b3b4a1f8836873ca69ca
chenaski <mail2chenaski@gmail.com>
2022-03-14 20:17:17 +0300

    feat(cli): use Joi for options validation

--- src/cli.js
+++ src/cli.js
@@ -1,12 +1,23 @@
 import { easyimg } from "./index.js";
 import { program } from "commander";
+import Joi from "joi";
 
 (async () => {
   program.option("-o, --out-dir <path>", "Output directory");
-
   program.parse();
 
   const options = program.opts();
+  const filePath = program.rawArgs[program.rawArgs.length - 1];
 
-  await easyimg(program.rawArgs[program.rawArgs.length - 1], options);
+  const schema = Joi.object({
+    filePath: Joi.string().required(),
+    outDir: Joi.string().required(),
+  });
+  const validationResult = schema.validate({ filePath, ...options });
+
+  if (validationResult.error) {
+    throw new Error(validationResult.error);
+  }
+
+  await easyimg(validationResult.value);
 })();
