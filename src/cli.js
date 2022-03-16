d30579cde81f10ac56dc51888537f6a675447f2f
chenaski <mail2chenaski@gmail.com>
2022-03-16 18:37:37 +0300

    feat(cli): use glob pattern over single file path

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import { easyimg } from "./index.js";
 import { program } from "commander";
 import Joi from "joi";
+import glob from "tiny-glob";
 import { logError } from "./logError.js";
 
 (async () => {
@@ -8,18 +9,27 @@ import { logError } from "./logError.js";
   program.parse();
 
   const options = program.opts();
-  const filePaths = program.rawArgs[program.rawArgs.length - 1];
+  const globPattern = program.rawArgs[program.rawArgs.length - 1];
 
   const schema = Joi.object({
-    filePaths: Joi.string().required(),
+    globPattern: Joi.string().required(),
     outDir: Joi.string().required(),
   });
-  const validationResult = schema.validate({ filePaths, ...options });
+  const validationResult = schema.validate({ globPattern, ...options });
 
   if (validationResult.error) {
     logError(validationResult.error);
     process.exit(1);
   }
 
-  await easyimg(validationResult.value);
+  const filePaths = await glob(validationResult.value.globPattern);
+
+  if (!filePaths.length) {
+    logError(
+      `No files matching the pattern "${globPattern}" were found.\nPlease check for typing mistakes in the pattern.`
+    );
+    process.exit(1);
+  }
+
+  await easyimg({ filePaths, ...validationResult.value });
 })();
